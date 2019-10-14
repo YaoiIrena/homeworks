49a99c84271d565a930e890e64954af3e23d94a8
YaoiIrena <yaoiirena87@gmail.com>
2019-10-14 21:04:49 +0500

    Выполненная задача №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -15,39 +15,16 @@ public class MyFirstProgram {
         //Формула AB = корень(AC^2 + BC^2), т.е. получается у нас 2 точки с (x1,y1) и (x2,y2)
         //Вариант 1
 
-        Point p1 = new Point();
-        Point p2 = new Point();
-        p1.p1 = 3;
-        p2.p2 = 6;
-        System.out.println(Math.sqrt(distance(p1, p2) + distance(p1, p2)));
+        Point point = new Point(3,6);
+        System.out.println("Расстояние от точки " + "=" + " " + Math.sqrt(point.distance() + point.distance()));
 
         //Вариант 2
 
-        PointStart x1 = new PointStart();
-        PointStart x2 = new PointStart();
-        PointEnd y1 = new PointEnd();
-        PointEnd y2 = new PointEnd();
-        x1.x1 = 2;
-        x2.x2 = 3;
-        y1.y1 = 3;
-        y2.y2 = 4;
+        PointStart pointStart = new PointStart(2,3);
+        PointEnd pointEnd = new PointEnd(3,4);
 
-        System.out.println("Расстояние от точки " + x1.x1 + " " + y1.y1 + " и " + x2.x2 + " " + y2.y2 + " = " + Math.sqrt((x2x1(x1, x2) + y2y1(y1, y2))));
+        System.out.println("Расстояние от точки " + pointStart.x1 + " " + pointEnd.y1 + " и " + pointStart.x2 + " " + pointEnd.y2 + " = " + Math.sqrt((pointStart.x2x1() + pointEnd.y2y1())));
 
-    }
-    public static double distance(Point p1, Point p2)
-    {
-        return Math.pow((p2.p2 - p1.p1), 2);
-    }
-
-    public static double x2x1(PointStart x1, PointStart x2)
-    {
-        return Math.pow((x2.x2 - x1.x1), 2);
-    }
-
-    public static double y2y1(PointEnd y1, PointEnd y2)
-    {
-        return Math.pow((y2.y2 - y1.y1), 2);
     }
 
     public static void hello(String somebody)
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -5,9 +5,14 @@ public class Point
     public double p1;
     public double p2;
 
-    /*public Point(double p1, double p2)
+    public Point(double p1, double p2)
     {
         this.p1 = p1;
         this.p2 = p2;
-    }*/
+    }
+
+    public double distance()
+    {
+        return Math.pow((this.p2 - this.p1), 2);
+    }
 }
