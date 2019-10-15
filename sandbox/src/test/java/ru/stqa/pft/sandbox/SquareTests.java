820f6a9d50df48306278a96480b574bc36ab8aed
YaoiIrena <yaoiirena87@gmail.com>
2019-10-15 17:46:13 +0500

    Задание №3

--- sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/SquareTests.java
@@ -10,5 +10,15 @@ public class SquareTests
     {
         Square s = new Square(5);
         Assert.assertEquals(s.area(), 25.0);
+
+        Point p1 = new Point(1,5);
+        Point p2 = new Point(3,4);
+        assert 0 < distance(p1,p2);
+        assert 1000 > distance(p1,p2);
+    }
+
+    private double distance(Point p1, Point p2)
+    {
+        return Math.sqrt(Math.pow((p2.getX() - p1.getX()), 2) + Math.pow((p2.getY() - p1.getY()), 2));
     }
 }
