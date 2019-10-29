0376204675fdaae808495d99778688441138b177
YaoiIrena <yaoiirena87@gmail.com>
2019-10-29 16:30:35 +0500

    Заполнение значениями по-умолчанию

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -9,7 +9,7 @@ public class GroupCreationTests extends TestBase {
   public void testGroupCreation() throws Exception {
     app.getNavigationHelper().gotoGroupPage(); //перейти на страницу групп
     app.getGroupHelper().initGroupCreation(); //инициал. созд. нов группы
-    app.getGroupHelper().fillGroupForm(new GroupData("test3", "test4", "test5")); //заполнить форму создания
+    app.getGroupHelper().fillGroupForm(new GroupData("test3", null, null)); //заполнить форму создания
     app.getGroupHelper().submitGroupCreation(); //создать
     app.getGroupHelper().returnToGroupPage(); //вернуться в список групп
     app.Logout(); //выход УЗ
