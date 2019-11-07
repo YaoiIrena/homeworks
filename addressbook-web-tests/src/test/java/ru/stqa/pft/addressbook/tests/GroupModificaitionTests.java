ef95ec4ec18a6bf07b285fe19d5b03ecd9bce1c5
YaoiIrena <yaoiirena87@gmail.com>
2019-11-08 00:09:20 +0500

    Модификация групп - сравнение эл-в по id'шнику

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificaitionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificaitionTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.util.HashSet;
 import java.util.List;
 
 public class GroupModificaitionTests extends TestBase {
@@ -19,10 +20,15 @@ public class GroupModificaitionTests extends TestBase {
         List<GroupData> before = app.getGroupHelper().getGroupList();
         app.getGroupHelper().selectGroup(before.size() - 1);
         app.getGroupHelper().initGroupModificaition();
-        app.getGroupHelper().fillGroupForm(new GroupData("1", "2", "3"));
+        GroupData group = new GroupData(before.get(before.size() - 1).getId(), "1", "2", "3");
+        app.getGroupHelper().fillGroupForm(group);
         app.getGroupHelper().submitGroupModification();
         app.getGroupHelper().returnToGroupPage();
         List<GroupData> after = app.getGroupHelper().getGroupList();
         Assert.assertEquals(after.size(), before.size());
+
+        before.remove(before.size() - 1);
+        before.add(group);
+        Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
     }
 }
