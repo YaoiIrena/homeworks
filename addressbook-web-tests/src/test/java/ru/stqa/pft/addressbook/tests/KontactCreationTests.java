d816bd57ef6556b2150ae33e872f2bc31bc80511
YaoiIrena <yaoiirena87@gmail.com>
2019-11-10 01:39:37 +0500

    использование лямбда выражения при создании контакта

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/KontactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/KontactCreationTests.java
@@ -18,13 +18,7 @@ public class KontactCreationTests extends TestBase {
     List<KontactData> after = app.getKontactHelper().getKontaktList();
     Assert.assertEquals(after.size(), before.size() + 1);
 
-    int max = 0;
-    for (KontactData g : after){
-      if (g.getId() > max){
-        max = g.getId();
-      }
-    }
-    contact.setId(max);
+    contact.setId(after.stream().max((o1, o2) -> Integer.compare(o1.getId(), o2.getId())).get().getId());
     before.add(contact);
     Assert.assertEquals(new HashSet<>(before), new HashSet<>(after));
     app.Logout();
