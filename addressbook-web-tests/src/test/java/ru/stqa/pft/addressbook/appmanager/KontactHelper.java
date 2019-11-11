cb35993d324bd9b89ca688dba3933537f979ec6d
YaoiIrena <yaoiirena87@gmail.com>
2019-11-11 15:30:01 +0500

    Доработка по заданию №9

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/KontactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/KontactHelper.java
@@ -110,7 +110,7 @@ public class KontactHelper extends HelperBase {
         List<WebElement> elements = wd.findElements(By.xpath("//tr[@name='entry']"));
         for (WebElement element : elements) {
             String firstname = element.findElement(By.xpath(".//td[3]")).getText();
-            String lastname = element.findElement(By.xpath("//td[2]")).getText();
+            String lastname = element.findElement(By.xpath(".//td[2]")).getText();
             int id = Integer.parseInt(element.findElement(By.tagName("input")).getAttribute("value"));
             KontactData contact = new KontactData(id, firstname, lastname, null, null, null,
                     null, null, null, null, null, null);
