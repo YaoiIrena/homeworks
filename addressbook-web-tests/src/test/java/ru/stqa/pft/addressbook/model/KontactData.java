2c82bffc9f3a90324da6f58941a3369330b6cb50
YaoiIrena <yaoiirena87@gmail.com>
2019-11-20 23:08:47 +0500

    (л 6.7) работа с форматом JSON д/групп и контактов

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/KontactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/KontactData.java
@@ -1,20 +1,32 @@
 package ru.stqa.pft.addressbook.model;
 
+import com.google.gson.annotations.Expose;
+
 import java.io.File;
 import java.util.Objects;
 
 public class KontactData {
     private int id = Integer.MAX_VALUE;
+    @Expose
     private String firstname;
+    @Expose
     private String lastname;
+    @Expose
     private String address;
+    @Expose
     private String home;
+    @Expose
     private String mobile;
+    @Expose
     private String work;
     private String fax;
+    @Expose
     private String email;
+    @Expose
     private String email2;
+    @Expose
     private String email3;
+    @Expose
     private String group;
     private String allPhones;
     private String allEmails;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/KontactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/KontactCreationTests.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -17,7 +19,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 public class KontactCreationTests extends TestBase {
 
   @DataProvider
-  public Iterator<Object[]> validContacts() throws IOException {
+  public Iterator<Object[]> validContactsFromXml() throws IOException {
     BufferedReader reader = new BufferedReader(new FileReader(new File("resources/contacts.xml")));
     String xml = "";
     String line = reader.readLine();
@@ -31,7 +33,21 @@ public class KontactCreationTests extends TestBase {
     return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
   }
 
-  @Test(dataProvider = "validContacts")
+  @DataProvider
+  public Iterator<Object[]> validContactsFromJson() throws IOException {
+    BufferedReader reader = new BufferedReader(new FileReader(new File("resources/contacts.json")));
+    String json = "";
+    String line = reader.readLine();
+    while (line != null){
+      json += line;
+      line = reader.readLine();
+    }
+    Gson gson = new Gson();
+    List<KontactData> contacts = gson.fromJson(json, new TypeToken<List<KontactData>>(){}.getType());
+    return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+  }
+
+  @Test(dataProvider = "validContactsFromJson")
   public void testKontactCreation(KontactData contact) throws Exception {
     Contacts before = app.contact().all();
     app.contact().create(contact, true);
