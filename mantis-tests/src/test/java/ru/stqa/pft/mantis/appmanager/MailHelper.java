634f2117b1e803177b9fe0ba91a7a89e9bd8ea1d
YaoiIrena <yaoiirena87@gmail.com>
2019-12-12 01:17:54 +0500

    Внесение изменений по лекции 8.6

---
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/MailHelper.java
@@ -0,0 +1,58 @@
+package ru.stqa.pft.mantis.appmanager;
+
+import org.subethamail.wiser.Wiser;
+import org.subethamail.wiser.WiserMessage;
+import ru.stqa.pft.mantis.model.MailMessage;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class MailHelper {
+    private ApplicationManager app;
+    private final Wiser wiser;
+
+    public MailHelper(ApplicationManager app) {
+        this.app = app;
+        wiser = new Wiser(); //объект почтового сервера
+    }
+
+    public List<MailMessage> waitForMail(int count, long timeout) throws InterruptedException {
+        long start = System.currentTimeMillis();
+        while (System.currentTimeMillis() < start + timeout){
+            if (wiser.getMessages().size() >= count){
+                return wiser.getMessages().stream().map((m) -> toModelMail(m)).collect(Collectors.toList());
+                }
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+        throw new Error("No mail ^___^");
+    }
+
+    public static MailMessage toModelMail(WiserMessage m) {
+       try {
+           MimeMessage mn = m.getMimeMessage();
+           return new MailMessage(mn.getAllRecipients()[0].toString(), (String) mn.getContent());
+            } catch (MessagingException e) {
+                e.printStackTrace();
+                return null;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return null;
+       }
+    }
+
+    public void start(){
+        wiser.start();
+    }
+
+    public void stop(){
+        wiser.stop();
+    }
+
+}
