91504fb5e3a7c20cc0654ea37a8f5417cfc30b47
YaoiIrena <yaoiirena87@gmail.com>
2019-12-13 02:33:13 +0500

    Правки по заданию 16

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddDeleteContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddDeleteContactTests.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 import ru.stqa.pft.addressbook.model.KontactData;
 
 public class AddDeleteContactTests extends TestBase {
@@ -24,6 +25,12 @@ public class AddDeleteContactTests extends TestBase {
 
     @Test
     public void testAddDeleteContact() {
+        if (app.db().selectGroupForDeleteContact() == null) {
+            app.contact().create(new KontactData().withFirstname("test1").withLastname("test2").withAddress("test3")
+                    .withHome("+7123456").withMobile("+713467").withWork("+75678").withFax("+72456")
+                    .withEmail("e@mail.ru").withEmail2("e2@mail.ru").withEmail3("e3@mail.ru"), true);
+        }
+
         KontactData contact = app.db().contacts().iterator().next();
         GroupData group = app.db().groups().iterator().next();
 
@@ -37,6 +44,15 @@ public class AddDeleteContactTests extends TestBase {
             Assert.assertFalse(contact.getGroups().contains(group));
         }
 
+        Groups groups = app.db().groups();
+        GroupData foundGroup = app.db().selectedGroup(groups, contact);
+        if (foundGroup != null) {
+            app.contact().returnToHomePage();
+            app.contact().create(new KontactData().withFirstname("test1").withLastname("test2").withAddress("test3")
+                    .withHome("+7123456").withMobile("+713467").withWork("+75678").withFax("+72456")
+                    .withEmail("e@mail.ru").withEmail2("e2@mail.ru").withEmail3("e3@mail.ru"), true);
+        } //находим только что добавленный контакт c max id и добавляем в группу
+        app.db().selectContactWithMaxId();
         app.contact().selectKontactById(contact.getId());
         // добавляем контакт в группу group.getName()
         app.contact().selectGroup(group);
