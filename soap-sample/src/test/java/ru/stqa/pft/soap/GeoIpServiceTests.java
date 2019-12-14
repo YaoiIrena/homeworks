e5a84b8a89a0249654a6196fb9118c09ba0adbbe
YaoiIrena <yaoiirena87@gmail.com>
2019-12-14 23:34:17 +0500

    Изменения по лекции 9.2

---
+++ soap-sample/src/test/java/ru/stqa/pft/soap/GeoIpServiceTests.java
@@ -0,0 +1,15 @@
+package ru.stqa.pft.soap;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GeoIpServiceTests {
+
+    @Test
+    public void testMyIp(){
+        GeoIP geoIP = new GeoIPService().getGeoIPServiceSoap12().getGeoIP("172.68.11.81");
+        Assert.assertEquals(geoIP.getCountryCode(), "RUS");
+    }
+}
