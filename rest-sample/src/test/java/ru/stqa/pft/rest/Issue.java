8e439bf0ea1581a04310b04f1a645ce8f779bc6d
YaoiIrena <yaoiirena87@gmail.com>
2019-12-15 20:51:06 +0500

    Изменения по л 9.4

---
+++ rest-sample/src/test/java/ru/stqa/pft/rest/Issue.java
@@ -0,0 +1,52 @@
+package ru.stqa.pft.rest;
+
+import java.util.Objects;
+
+public class Issue {
+
+    private int id;
+    private String subject;
+    private String describtion;
+
+    public int getId() {
+        return id;
+    }
+
+    public Issue withId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public Issue withSubject(String subject) {
+        this.subject = subject;
+        return this;
+    }
+
+    public String getDescription() {
+        return describtion;
+    }
+
+    public Issue withDescription(String describtion) {
+        this.describtion = describtion;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Issue issue = (Issue) o;
+        return id == issue.id &&
+                Objects.equals(subject, issue.subject) &&
+                Objects.equals(describtion, issue.describtion);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, subject, describtion);
+    }
+}
