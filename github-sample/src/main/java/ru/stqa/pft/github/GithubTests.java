30358249bb4828c69b3218d432bfda0cbf10a28e
YaoiIrena <yaoiirena87@gmail.com>
2019-12-16 23:35:58 +0500

    Изменения по л 9.6

---
+++ github-sample/src/main/java/ru/stqa/pft/github/GithubTests.java
@@ -0,0 +1,19 @@
+package ru.stqa.pft.github;
+
+import com.google.common.collect.ImmutableMap;
+import com.jcabi.github.*;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class GithubTests {
+
+    @Test
+    public void testCommits() throws IOException {
+        Github github = new RtGithub("0a0f7b640ef1679a01a697e13868adbee0464cf3");
+        RepoCommits commits = github.repos().get(new Coordinates.Simple("YaoiIrena", "homeworks")).commits();
+        for (RepoCommit commit : commits.iterate(new ImmutableMap.Builder<String, String>().build())) {
+            System.out.println(new RepoCommit.Smart(commit).message());
+        }
+    }
+}
