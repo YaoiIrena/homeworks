9868803923437652e3ce25e0f256511664fb6fec
YaoiIrena <yaoiirena87@gmail.com>
2019-12-16 21:35:57 +0500

    Изменения по заданию 16 (окончательные)

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddDeleteContactTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddDeleteContactTests.java
@@ -21,45 +21,88 @@ public class AddDeleteContactTests extends TestBase {
                     .withHome("+7123456").withMobile("+713467").withWork("+75678").withFax("+72456")
                     .withEmail("e@mail.ru").withEmail2("e2@mail.ru").withEmail3("e3@mail.ru"), true);
         }
+
+        app.contact().returnToHomePage();
     }
 
     @Test
-    public void testAddDeleteContact() {
-        if (app.group().selectGroupForDeleteContact() == null) {
+    public void testAddContact() {
+        KontactData foundContact = null;
+        GroupData foundGroup = null;
+        for (KontactData contact : app.db().contacts()) {
+            Groups contactGroups = contact.getGroups();
+            for (GroupData group : app.db().groups()) {
+                if (!contactGroups.contains(group)) {
+                    foundContact = contact;
+                    foundGroup = group;
+                    break;
+                }
+            }
+            if (foundContact != null) {
+                break;
+            }
+        }
+
+        if (foundContact == null) {
+            // если нет подходящего контакта, то создаем его
             app.contact().create(new KontactData().withFirstname("test1").withLastname("test2").withAddress("test3")
                     .withHome("+7123456").withMobile("+713467").withWork("+75678").withFax("+72456")
                     .withEmail("e@mail.ru").withEmail2("e2@mail.ru").withEmail3("e3@mail.ru"), true);
-        }
 
-        KontactData contact = app.db().contacts().iterator().next();
-        GroupData group = app.db().groups().iterator().next();
+            // находим только что добавленный контакт c max id и добавляем в группу
+            KontactData contact = app.db().selectContactWithMaxId();
+            app.contact().selectKontactById(contact.getId());
 
-        if (contact.getGroups().contains(group)) {
-            // удаляем контакт из группы
-            app.contact().selectContactFromGroup(contact, group);
-            app.contact().deleteFromGroup();
-            app.contact().returnToHomePage();
+            GroupData firstGroup = app.db().groups().iterator().next();
+            // добавляем контакт в группу
+            app.contact().selectGroup(firstGroup);
+            app.contact().addInGroup();
 
             contact = app.db().getContactById(contact.getId());
-            Assert.assertFalse(contact.getGroups().contains(group));
+            Assert.assertTrue(contact.getGroups().contains(firstGroup));
+        } else {
+            // выбираем контакт
+            app.contact().selectKontactById(foundContact.getId());
+            // добавляем контакт в группу
+            app.contact().selectGroup(foundGroup);
+            app.contact().addInGroup();
+
+            foundContact = app.db().getContactById(foundContact.getId());
+            Assert.assertTrue(foundContact.getGroups().contains(foundGroup));
         }
+    }
 
-        Groups groups = app.db().groups();
-        GroupData foundGroup = app.group().selectedGroup(groups, contact);
-        if (foundGroup != null) {
+    @Test
+    public void testDeleteContact() {
+        KontactData contact = app.db().contacts().stream().filter(kontact -> kontact.getGroups().size() > 0).findFirst().orElse(null);
+        if (contact == null) {
+            KontactData firstContact = app.db().contacts().iterator().next();
+            GroupData firstGroup = app.db().groups().iterator().next();
+
+            // выбираем контакт
+            app.contact().selectKontactById(firstContact.getId());
+            // добавляем контакт в группу
+            app.contact().selectGroup(firstGroup);
+            app.contact().addInGroup();
             app.contact().returnToHomePage();
-            app.contact().create(new KontactData().withFirstname("test1").withLastname("test2").withAddress("test3")
-                    .withHome("+7123456").withMobile("+713467").withWork("+75678").withFax("+72456")
-                    .withEmail("e@mail.ru").withEmail2("e2@mail.ru").withEmail3("e3@mail.ru"), true);
-        } //находим только что добавленный контакт c max id и добавляем в группу
-        app.db().selectContactWithMaxId();
-        app.contact().selectKontactById(contact.getId());
-        // добавляем контакт в группу group.getName()
-        app.contact().selectGroup(group);
-        app.contact().addInGroup();
-        app.contact().returnToHomePage();
 
-        contact = app.db().getContactById(contact.getId());
-        Assert.assertTrue(contact.getGroups().contains(group));
+            firstContact = app.db().getContactById(firstContact.getId());
+            Assert.assertTrue(firstContact.getGroups().contains(firstGroup));
+
+            // удаляем контакт из группы
+            app.contact().selectContactFromGroup(firstContact, firstGroup);
+            app.contact().deleteFromGroup();
+
+            firstContact = app.db().getContactById(firstContact.getId());
+            Assert.assertFalse(firstContact.getGroups().contains(firstGroup));
+        } else {
+            GroupData firstGroup = contact.getGroups().iterator().next();
+            // удаляем контакт из группы
+            app.contact().selectContactFromGroup(contact, firstGroup);
+            app.contact().deleteFromGroup();
+
+            contact = app.db().getContactById(contact.getId());
+            Assert.assertFalse(contact.getGroups().contains(firstGroup));
+        }
     }
 }
